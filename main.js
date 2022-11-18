52aea162690623cda6a10f3dbae13c487938b122
Sarah <vanwars@gmail.com>
2022-11-18 10:35:32 -0500

    prototype updated

--- main.js
+++ main.js
@@ -1,28 +1,75 @@
-const showSection = (fileName, idx, doAnimation=true) => {
-    console.log(`#panel-${idx}`);
+let storedHash;
+const pages = [];
+
+const showSection = (fileName, idx) => {
+    console.log(fileName);
     const slideEl = document.querySelector(`#panel-${idx}`);
     const containerEl = document.querySelector('.slides-container');
     fetch(`./templates/${fileName}`)
         .then(response => response.text())
         .then(html => {
             slideEl.innerHTML = html;
-            if (doAnimation) {
-                containerEl.scrollLeft = slideEl.clientWidth * idx;
-            }
+            document.body.scrollTo(0, 0);
+            window.scrollTo(0, 0);
+            containerEl.scrollLeft = slideEl.clientWidth * idx;
+            document.querySelector('nav').classList.remove('show');
         })
 };
 
-const initPanels = () => {
-    const n = document.querySelectorAll('ul li').length;
+const initPage = () => {
+    initNavigation();
+    loadFirstPage();
+};
+
+const initNavigation = () => {
     const containerEl = document.querySelector('.slides-container');
-    for (let i = 0; i < n; i++ ) {
+    const links = document.querySelectorAll('ul a');
+    links.forEach((link, idx) => {
+        pages.push(link.href.split('#')[1]);
+        link.dataset.index = idx;
         const template = `
-            <section class='slide' id="panel-${i}"></section>
+            <section class='slide' id="panel-${idx}"></section>
         `;
         containerEl.insertAdjacentHTML('beforeend', template);
-    }
+        // link.addEventListener('click', showSection);
+    })
     containerEl.scrollLeft = 0;
+};
+
+const loadFirstPage = () => {
+    storedHash = window.location.hash;
+    const tokens = storedHash.split('#');
+    console.log(tokens, pages);
+    if (tokens.length < 2 || tokens[1].length === 0 || !pages.includes(tokens[1])) {
+        window.location.href = '#main';
+    } else {
+        showPage();
+    }
 }
 
-initPanels();
-showSection('main.html', 0, false);
+const showPage = () => {
+    const fileName = storedHash.replace('#', '') + '.html';
+    document.querySelectorAll('ul a').forEach((el, idx) => {
+        if (el.href == window.location.href) {
+            showSection(fileName, idx);
+        }
+    })
+};
+
+const toggleMenu = ev => {
+    ev.preventDefault();
+    document.querySelector('nav').classList.toggle('show');
+}
+
+initPage();
+
+// trigger page load:
+// document.querySelector(`#panel-0`).click();
+
+window.setInterval(function () {
+    if (window.location.hash != storedHash) {
+        storedHash = window.location.hash;
+        showPage();
+    }
+}, 100);
+
